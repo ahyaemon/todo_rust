02d5853cb43c489898f173742b1ee6a0b736c8c4
ahyaemon <iamahyaemon@gmail.com>
2024-05-14 20:31:57 +0900

    リファクタリング

--- back/src/application/list_todo_summaries/list_todo_summaries_use_case.rs
+++ back/src/application/list_todo_summaries/list_todo_summaries_use_case.rs
@@ -1,12 +1,22 @@
 use std::sync::Arc;
 use serde::Serialize;
 use crate::application::todo_repository::TodoRepository;
+use crate::domain::todo::Todo;
 
 #[derive(Serialize)]
 pub struct TodoSummary {
     id: String,
     title: String,
     description_summary: String,
+}
+
+impl From<&Todo> for TodoSummary {
+    fn from(todo: &Todo) -> Self {
+        let title = todo.title.to_string();
+        let short_description = &todo.description[..24];
+        let description_summary = format!("{}...", short_description);
+        TodoSummary { id: todo.id.clone(), title, description_summary }
+    }
 }
 
 pub struct ListTodoSummariesUseCase<T: TodoRepository> {
@@ -21,11 +31,6 @@ impl<T: TodoRepository> ListTodoSummariesUseCase<T> {
 
     pub fn act(&self) -> Vec<TodoSummary> {
         let todos = self.todo_repository.list();
-        todos.iter().map(|todo| {
-            let title = todo.title.to_string();
-            let short_description = &todo.description[..24];
-            let description_summary = format!("{}...", short_description);
-            TodoSummary { id: todo.id.clone(), title, description_summary }
-        }).collect()
+        todos.iter().map(TodoSummary::from).collect()
     }
 }
